bf714d3faaa88a1101ea32fb6332a9203bf40890
Matias-Estay <matias.estay@hotmail.com>
2022-11-30 03:34:55 -0300

    arreglo de navbar

--- resources/js/components/Globals/PokemonInfo.jsx
+++ resources/js/components/Globals/PokemonInfo.jsx
@@ -176,10 +176,10 @@ export default function PokemonInfo(props) {
                 <img className='sprite' src={'/images/sprites/'+dataP.sprite}/>
                 {dataP.nombre}
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent-pokemon" aria-controls="navbarSupportedContent-pokemon" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-pokemon">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     {props.handleClose!=null?
                         <li class="nav-item">
